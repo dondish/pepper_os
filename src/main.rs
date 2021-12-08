8467ce65dad80109a394623d3dce3c7cafcf3ca8
Oded Shapira <odedshapira1@protonmail.com>
2021-12-08 18:42:54 +0200

    Add testing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 mod vga_buffer;
+mod serial;
 
 /// This function is called on panic.
 #[panic_handler]
@@ -11,11 +15,45 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &str = "Oded Shapira\nCyber";
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("{}", HELLO);
+    println!("Starting Pepper OS");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
